e499e01243c3344e41a940f1ad89a4b2bfc47350
Sayan Deb <keepimaginingsayan@gmail.com>
2025-03-25 17:00:26 +0530

    Modified and updated code

--- sqlchain.py
+++ sqlchain.py
@@ -5,27 +5,31 @@ import openai
 import pandas as pd
 # from langchain.agents import create_pandas_dataframe_agent
 from langchain_experimental.agents import create_pandas_dataframe_agent
-
+from dotenv import load_dotenv 
 from langchain.chat_models import ChatOpenAI
 from langchain.agents.agent_types import AgentType
 
 # df: pd.DataFrame = pd.read_csv("./Uscensus/ahs-cab2014-chhattisgarh-durg.csv")
 df: pd.DataFrame = pd.read_csv("./llm_finetuning/acs_data.csv")
 
-with open("openai_api_key.txt", "r") as f:
-    API_KEY = f.read()
+load_dotenv()
 
-os.environ["OPENAI_API_KEY"] = API_KEY
+# with open("openai_api_key.txt", "r") as f:
+#     API_KEY = f.read()
+
+# os.environ["OPENAI_API_KEY"] = API_KEY
+API_KEY = os.getenv("OPENAI_API_KEY")
 
 class SequentialChain:
     """Class file for SQL and Dataframe"""
     
     def __init__(self, verbose: bool=True) -> None:
         self.agent = create_pandas_dataframe_agent(
-        ChatOpenAI(openai_api_key=os.environ["OPENAI_API_KEY"], temperature=0, model="gpt-4"),
+        ChatOpenAI(openai_api_key=API_KEY, temperature=0, model="gpt-4"),
         df,
         verbose=verbose,
         agent_type=AgentType.OPENAI_FUNCTIONS,
+        allow_dangerous_code=True,
         )
         
     def get_agent_answer(self, query: str) -> str:
